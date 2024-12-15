e5e8334d4d90ebcbae2c7c9376f2438c7af10a30
MeFerdi <ferdinandodhis254@gmail.com>
2024-12-15 06:53:53 +0300

    chore: setup file system for deployment

---
+++ initializers/syncDb.go
@@ -0,0 +1,16 @@
+package initializers
+
+import (
+	"log"
+	"savebackend/models"
+)
+
+// initializers/syncDb.go
+func SyncDb() {
+	log.Println("Starting database migration...")
+	err := DB.AutoMigrate(&models.User{}, &models.Satelitte{}, &models.DonorBlood{}, &models.Hospital{}, &models.Regional{}, &models.Emergency{}, &models.HospitalRequest{})
+	if err != nil {
+		log.Fatalf("Failed to migrate database: %v", err)
+	}
+	log.Println("Database migration completed successfully")
+}
